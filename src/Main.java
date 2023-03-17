767fe6adb2ef2869727d168281576cf31be2f649
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 15:59:41 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in alla rader från en fil och spara alla de rader som
+        // innehåller en viss text i en annan fil
+
+
+    }
+}
